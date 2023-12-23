8de8771263e2026e0493605bbb214742ba6137b2
Zainab Saad <zsaad.bese21seecs@seecs.edu.pk>
2023-12-24 01:37:01 +0500

    Add network bridge in docker-compose.yaml
    - additionally allow the usage of cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { authRouter } from './routes/auth.route.js';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(authRouter);
 
 const PORT = process.env.NODE_DOCKER_PORT;
